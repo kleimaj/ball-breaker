256b6a1332f657d3e8d184cbf8ce21ed8cb2c2da
kleimaj <jacobakleiman@gmail.com>
2021-01-17 00:02:55 -0800

    Adds cached AudioSource reference, adds serialized sfx clips

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,14 +7,20 @@ public class Ball : MonoBehaviour {
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+
     // state
     // distance between paddle and ball
     Vector2 paddleToBallVector;
     bool hasStarted = false;
 
+    // cached component references
+    AudioSource myAudioSource;
+
     // Start is called before the first frame update
     void Start() {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -38,7 +44,8 @@ public class Ball : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (hasStarted) {
-            GetComponent<AudioSource>().Play();
+            AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
         }
     }
 }
