e4d4c840cfb669de30e201ca67b50720a577f838
kleimaj <jacobakleiman@gmail.com>
2021-01-18 00:23:40 -0800

    Adds AutoPlay mode

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -9,6 +9,7 @@ public class GameSession : MonoBehaviour {
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool autoPlayEnabled;
 
     // state variables
     [SerializeField] int currentScore = 0;
@@ -41,6 +42,10 @@ public class GameSession : MonoBehaviour {
 
     public void DestroySelf() {
         Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled() {
+        return autoPlayEnabled;
     }
 
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    // config params
     [SerializeField] float screenWidthInUnits = 16f;
     [SerializeField] float minX = 1f;
     [SerializeField] float maxX = 15f;
+
+    // references
+    GameSession session;
+    Ball ball;
     // Start is called before the first frame update
     void Start() {
-        
+        session = FindObjectOfType<GameSession>();
+        ball = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update() {
-        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePos;
+    }
+
+    private float GetXPos() {
+        if (session.IsAutoPlayEnabled()) {
+            return ball.transform.position.x;
+        }
+        else {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
